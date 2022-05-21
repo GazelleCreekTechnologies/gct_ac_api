3b99dc59448a3d936ae65395808521671a94aacd
Lindani Ricardo Mabaso <mabasolindaniricardo@gmail.com>
2022-05-21 20:44:16 +0200

    update connection pool, instead of reconnecting

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = require('express')();
 const SwaggerJsDoc = require('swagger-jsdoc')
 const SwaggerUI = require('swagger-ui-express')
-const mysql = require('mysql');
+
+// Load the MySQL pool connection
+const pool = require('./data/config');
 
 const PORT = process.env.PORT || 8080
 
@@ -13,16 +15,20 @@ console.log('Hello, world!');
 const swaggerOptions = {
     swaggerDefinition: {
         info: {
-            title: 'Access Control API',
-            description: "Access Control Endpoint",
+            title: 'Gazelle Creek Technologies - Access Control API',
+            description: "This is the endpoint to get the results and also update from the pi to the database, \
+                          and also to and fro admin website interface. This is where we get admin http://access-control.gctza.co.za/ \
+                         url, in this dashboard we view all the access control stats and summary of the users. This is were we also insert \
+                         and manage the tenants and visitors, also extract timesheet records.\
+                        ",
             contact: {
-                name: "Lindani Mabaso"
+                name: "Lindani Mabaso | Gazelle Creek Technologies"
             },
             servers: ["http://localhost:8080"]
         }
     },
     // ['.routes/*.js']
-    apis: ["src/index.js"]
+    apis: ["src/*.js"]
 };
 
 const swaggerDocs = SwaggerJsDoc(swaggerOptions);
@@ -30,48 +36,55 @@ const swaggerDocs = SwaggerJsDoc(swaggerOptions);
 app.use(express.json())
 app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(swaggerDocs));
 
-//Database Connection
-// mysql://gctzapmw_access_control:password@host:3306/gctzapmw_access_control_db
-const connection = mysql.createConnection({
-    host: "host",
-    user: "gctzapmw_access_control",
-    password: "password",
-    database: "gctzapmw_access_control_db",
-    connectionLimit: 10
-})
+//============================================= GET =================================
 
-connection.connect((err) => {
-    if(err) throw err;
-    console.log('Connected to MySQL Server!');
-});
-
-connection.query(`select * from Persons`, (err, result, fields) => {
-    if (err) {
-        console.log('Error connecting');
-
-        return console.log(err);
-    }
-    return console.log(result);
-})
-
-connection.end()
-
-//Test
+//Get All The User Details
 /**
  * @swagger
- * /test:
+ * /userdetails:
  *  get:
- *    description: Use to request all test
+ *    description: Get all the user details from the UserTbl
  *    responses:
  *      '200':
  *        description: A successful response
+ *      '404':
+ *        description: no users found response
  */
-app.get('/test',(req,res) =>{
-    res.status(200).send({
-        'test':'This',
-        'sixe': 'large'
+app.get('/userdetails',(request, response) =>{
+
+    pool.query(`select * from UserTbl`, (err, result) => {
+        if (err) {
+            return err;
+        }
+        response.send(result);
     })
+   
 });
+
+//Get All The Visitors
+/**
+ * @swagger
+ * /visitors:
+ *  get:
+ *    description: Get all the visitor details from the VisitorTbl
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *      '404':
+ *        description: no visitors found response
+ */
+ app.get('/visitors',(request, response) =>{
+
+    pool.query(`select * from VisitorTbl`, (err, result) => {
+        if (err) {
+            return err;
+        }
+        response.send(result);
+    })
+   
+});
+
+//============================================= UPDATE =================================
 
 /**
  * @swagger
@@ -106,6 +119,14 @@ app.post('/test/:id',(req,res) =>{
     })
 });
 
+//============================================= POST / INSERT =================================
+
+//============================================= DELETE ========================================
+
+//============================================= START =========================================
 app.listen(PORT, () => { 
-    console.log(`Its alive on http://localhost:${PORT}`);
+    var host = app
+    var port = PORT
+    
+    console.log("App listening at http://%s:%s", host, port)
 })
